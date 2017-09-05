521550581cc66a101eff3127f2266bc50b31561c
Jaakko Kangasharju <jkan@futurice.com>
2017-09-05 16:26:09 +0200

    Bring in Cocoapods and fix signing and provisioning

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '10.0'
+use_frameworks!
+
+target 'xcconfig-sample' do
+
+  # Pods for xcconfig-sample
+  pod 'SnapKit', '~> 3.2'
+
+end
---
+++ Pods/Target Support Files/Pods-xcconfig-sample/Pods-xcconfig-sample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_xcconfig_sample : NSObject
+@end
+@implementation PodsDummy_Pods_xcconfig_sample
+@end
